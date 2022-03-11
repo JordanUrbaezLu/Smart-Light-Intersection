a3bb2884f885b6247479874eadc83b130c7cd0ee
JordanUrbaezLu <93447469+JordanUrbaezLu@users.noreply.github.com>
2022-03-10 22:16:51 -0500

    Add files via upload

---
+++ AI Project Code/intersection.h
@@ -0,0 +1,36 @@
+//
+//  intersection.h
+//  Artificial Intelligence Project
+//
+//  Created by Jason Moynihan on 11/21/20.
+//  Copyright © 2020 Jason Moynihan. All rights reserved.
+//
+
+#ifndef intersection_h
+#define intersection_h
+
+#include "light.h"
+
+using namespace std;
+
+class Intersection
+{
+public:
+	Intersection();
+	Light getLight(int index);
+	void runCycles(int timePeriod);
+	void runCycle();
+	void startAC(int timePeriod);
+	void startBD(int timePeriod);
+	void startA(int timePeriod);
+	void startC(int timePeriod);
+	void startB(int timePeriod);
+	void startD(int timePeriod);
+	void adjust();
+	vector<Light> _lights;
+private:
+	int cycleCount;
+};
+
+
+#endif /* intersection_h */
---
+++ AI Project Code/light.h
@@ -0,0 +1,41 @@
+//
+//  light.h
+//  Artificial Intelligence Project
+//
+//  Created by Jason Moynihan on 11/21/20.
+//  Copyright © 2020 Jason Moynihan. All rights reserved.
+//
+
+#ifndef light_h
+#define light_h
+
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Light
+{
+public:
+	Light();
+	void setNewTime(/*string color,*/ double time);
+	void setState(string color);
+	string getState();
+	void addCarsWaiting(int num);
+	void letCarThrough();
+	double getTime(/*string color*/);
+	int getCarsWaiting();
+	int score;
+private:
+	//map<string, int> _cyclePeriods;
+	//double greenTimeRemaining;
+	double greenTime;
+	int TimeLeft;
+	int _carsWaiting;
+	string state;
+	int currHour;
+
+};
+
+#endif /* light_h */
